e61a1aa5cf7a0b51abb1c61b787d24dc5d7e946e
Tyler Johnston <johnstontyler@protonmail.com>
2025-04-26 00:03:17 -0600

    trying another env var fix for vercel

---
+++ scripts/config.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const targetPath = './src/environments/environment.ts';
+
+// Environment.ts content
+const envConfigFile = `
+export const environment = {
+  production: true,
+  supabaseUrl: '${process.env.NG_ENV_SUPABASE_URL}',
+  supabaseAnonKey: '${process.env.NG_ENV_SUPABASE_ANON_KEY}'
+};
+`;
+
+fs.writeFile(targetPath, envConfigFile, (err) => {
+  if (err) {
+    console.error('Error writing environment.ts:', err);
+    throw err;
+  } else {
+    console.log('Environment.ts file generated successfully.');
+  }
+});
